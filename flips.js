e9b48e66271c53ced955f4003781a1a1d8c6939b
Elaine Dong <elaine13@email.unc.edu>
2022-02-11 16:45:03 -0500

    fixed flips script

--- flips.js
+++ flips.js
@@ -2,6 +2,7 @@ import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from 'minimist'
 
 const args = minimist(process.argv.slice(2))
-const flips = coinFlips(args.number || 1)
+var flips
+args.number == undefined ? flips = coinFlips(1) : flips = coinFlips(args.number)
 console.log(flips)
 console.log(countFlips(flips))
