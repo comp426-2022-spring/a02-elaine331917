28e70a53aab72515af6e5052c9ede99e97ffb31b
Elaine Dong <elaine13@email.unc.edu>
2022-02-11 16:52:24 -0500

    fixed guessing script

--- guess-flip.js
+++ guess-flip.js
@@ -4,11 +4,9 @@ import minimist from 'minimist'
 const args = minimist(process.argv.slice(2))
 
 if (args.call == undefined) {
-    console.log({
-        Error: 'no input.',
-        Usage: 'node guess-flip --call=[heads|tails]'
-    })
-} else {
+    console.log('Error: no input.\nUsage: node guess-flip --call=[heads|tails]')
+} else if (args.call == 'heads' || args.call == 'tails') {
     console.log(flipACoin(args.call))
+} else {
+    console.log('Error: invalid input.\nUsage: node guess-flip --call=[heads|tails]')
 }
-
